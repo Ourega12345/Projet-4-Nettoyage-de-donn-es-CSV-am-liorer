72802aa4d258f22517d285572214499bade8ebef
Dago Ourega <narutoluffyr@gmail.com>
2026-04-23 19:05:31 -0400

    Update NettoyeurDonnees.java
    supprimé le valeur manquante

--- Ourega_Isidore/NettoyeurDonnees.java
+++ Ourega_Isidore/NettoyeurDonnees.java
@@ -3,7 +3,7 @@ public class NettoyeurDonnees {
     public String traiterCellule(String texte) {
         // 1. Gérer les valeurs manquantes
         if (texte == null || texte.trim().isEmpty()) {
-            return "VALEUR_MANQUANTE";
+            return null;
         }
         // 2. Standardiser (Majuscules)
         return texte.trim().toUpperCase();
